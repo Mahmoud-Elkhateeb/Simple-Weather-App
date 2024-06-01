c650a5a2ebe34400d9b20e57e90c943cb072f9fc
Mahmoud-Elkhateeb <elkhateebmahmoud84@gmail.com>
2024-06-01 08:45:36 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+function displayCityName() {
+
+    var cityInput = document.getElementById("city-input");
+    var cityName = document.getElementById("city-name");
+    cityName.textContent = "City: " + cityInput.value;
+
+    let apikey = `https://api.weatherapi.com/v1/current.json?key=eaf9e14a0e904967b1e31755240106&q=${cityInput.value}&aqi=no`
+    fetch(apikey)
+        .then((req) => req.json().finally())
+        .then((data) => {
+            // 
+            // console.log(data);
+            // 
+            var temp = document.getElementById("temperature");
+            temp.innerHTML = `${data.current.temp_c} cْ`;
+            // 
+            var icon = document.getElementById("weather-icon");
+            icon.src = data.current.condition.icon
+        })
+}
+//Eng\ Mahmoud Elkhateeb
